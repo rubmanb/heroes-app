bbc489ba96e57857c901c9b065a572352cfdfbca
rubmanb <rubenm1386@gmail.com>
2024-05-25 00:34:51 +0200

    adding every path we will need

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
-
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  exports: []
 })
 export class MaterialModule { }
