cfa55cf0b63127aec7a4e93f693a1804863e76a3
rubmanb <rubenm1386@gmail.com>
2024-05-29 17:01:03 +0200

    adding images and front end of heroes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,3 +1,4 @@
+import { Hero } from './interfaces/hero.interface';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -8,6 +9,8 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { MaterialModule } from '../material/material.module';
+import { CardComponent } from './components/card/card.component';
+import { HeroPipe } from './pipes/hero-image/hero-image.pipe';
 
 
 @NgModule({
@@ -16,7 +19,9 @@ import { MaterialModule } from '../material/material.module';
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    CardComponent,
+    HeroPipe
   ],
   imports: [
     CommonModule,
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environment } from '../../../environments/environments';
 
@@ -14,5 +14,13 @@ export class HeroesService {
 
   getHeroes(): Observable<Hero[]>{
     return this.http.get<Hero[]>(`${this._baseUrl}/heroes`);
+  }
+
+
+  getHeroById(id: string): Observable<Hero|undefined>{
+    return this.http.get<Hero>(`${this._baseUrl}/heroes/${id}`)
+    .pipe(
+      catchError(error => of(undefined))
+    );
   }
 }
