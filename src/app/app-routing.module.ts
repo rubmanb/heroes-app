c14b1f8139bddb37f77ea8fc99a160fed5dd86a4
rubenmb <ruben.mansanet@vurbis.com>
2024-05-31 09:13:28 +0200

    Adding another guard for the logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,35 +2,39 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 import { canActivateGuard, canMatchGuard } from './auth/guards/auth.guard';
+import { canActivateGuard2, canMatchGuard2 } from './auth/guards/public.guard';
 
 const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [canActivateGuard2],
+    canMatch: [canMatchGuard2],
   },
   {
     path: 'heroes',
-    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule),
+    loadChildren: () =>
+      import('./heroes/heroes.module').then((m) => m.HeroesModule),
     canActivate: [canActivateGuard],
-    canMatch: [canMatchGuard]
+    canMatch: [canMatchGuard],
   },
   {
     path: '404',
-    component: Error404PageComponent
+    component: Error404PageComponent,
   },
   {
     path: '',
     redirectTo: 'heroes',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: '**',
-    redirectTo: '404'
-  }
+    redirectTo: '404',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/auth/guards/public.guard.ts
@@ -0,0 +1,50 @@
+import { inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  CanMatchFn,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+} from '@angular/router';
+import { Observable, map, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+const checkAuthStatus = (): boolean | Observable<boolean> => {
+  //se inyectan el AuthService y el Router
+  const authService: AuthService = inject(AuthService);
+  const router: Router = inject(Router);
+
+  return authService.checkAuthentication().pipe(
+    tap((isAuthenticated) => {
+      if (isAuthenticated) {
+        router.navigate(['./']);
+      }
+    }),
+    map((isAthenticated) => !isAthenticated)
+  );
+};
+
+//No hay necesidad de crear una clase, simplemente definiendo una función flecha y exportándola podemos utilizar sus funcionalidades de guard en el app-routing
+export const canActivateGuard2: CanActivateFn = (
+  //Hay que tener en cuenta el tipado CanActiveFn
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  console.log('CanActivate');
+  console.log({ route, state });
+
+  return checkAuthStatus();
+};
+
+export const canMatchGuard2: CanMatchFn = (
+  //Tipado CanMatchFN
+  route: Route,
+  segments: UrlSegment[]
+) => {
+  console.log('CanMatch');
+  console.log({ route, segments });
+
+  return checkAuthStatus();
+};
