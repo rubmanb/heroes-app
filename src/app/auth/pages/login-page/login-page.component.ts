b3cba8fa531f5dff27cd5f36e78970e63cd3beb1
rubmanb <rubenm1386@gmail.com>
2024-05-31 07:02:21 +0200

    adding guards and localstorage

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styles: ``
 })
-export class LoginPageComponent {
+export class LoginPageComponent{
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+
+
+  onLogin():void {
+    this.authService.login('ruben@asd.com', '1234')
+    .subscribe(
+      user => this.router.navigate(['/'])
+    );
+  }
+
+
 
 }
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,3 +1,6 @@
+import { Router } from '@angular/router';
+import { User } from '../../../auth/interfaces/user.interface';
+import { AuthService } from './../../../auth/services/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,9 +9,24 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class LayoutPageComponent {
+
   public sidenavitems=  [
     {label: 'Listado', icon: 'label', url: './list'},
     {label: 'Añadir', icon: 'add', url: './new-hero'},
     {label: 'Buscar', icon: 'search', url: './search'},
   ];
+
+  constructor(
+    private authService: AuthService,
+    private route: Router
+  ){}
+
+  get user(): User|undefined{
+    return this.authService.currentUser;
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.route.navigate(['/auth/login']);
+  }
 }
